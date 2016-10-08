aab718593ed2c81f019e1d4a055ef31f9a51f0a6
mcp1993 <1023042656@qq.com>
2016-10-08 18:09:59 +0800

    RecyclerView

---
+++ app/src/main/java/com/mcp1993/recyclerviewdemo/Movie.java
@@ -0,0 +1,43 @@
+package com.mcp1993.recyclerviewdemo;
+
+/**
+ * Created by mcp1993 on 2016/10/8 0008.
+ */
+
+public class Movie {
+
+    private String title, genre, year;
+
+    public Movie() {
+    }
+
+    public Movie(String title, String genre, String year) {
+        this.title = title;
+        this.genre = genre;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+}
